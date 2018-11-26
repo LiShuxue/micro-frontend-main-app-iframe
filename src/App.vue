2a52fd8fd40f1052780cb4edc24d47ac04f0507d
LiShuxue <1149926505@qq.com>
2018-11-26 16:56:19 +0800

    remove the prettier and add menu

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div class="app">
+    <el-menu
+      default-active="1"
+      class="nav"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1">
+        <i class="el-icon-menu"></i>
+        <span slot="title">Module A</span>
+      </el-menu-item>
+      <el-menu-item index="2" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">Module B</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span slot="title">Module C</span>
+      </el-menu-item>
+    </el-menu>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
+.app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+  .nav {
+    width: 15vw;
+    height: 100vh;
   }
 }
 </style>
